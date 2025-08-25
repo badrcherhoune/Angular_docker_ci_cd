29d1a0194e4be31fb3bf8845799ee632b393bfcc
badr cherhoune <badrcherhoune199@gmail.com>
2025-08-25 16:01:51 +0200

    clean code

--- src/app/user-stats/user-stats.component.ts
+++ src/app/user-stats/user-stats.component.ts
@@ -6,6 +6,22 @@ import { Component, type OnInit } from '@angular/core';
   templateUrl: './user-stats.component.html',
   styleUrl: './user-stats.component.css'
 })
-export class UserStatsComponent {
-
+export class UserStatsComponent implements OnInit {
+  ngOnInit(): void {
+    
+  }
+  selectedPeriod: string = 'Cette semaine';
+  selectedSector: string = 'Tous les secteurs';
+  selectedCompliance: string = 'Tous';
+  
+  periods: string[] = ['Cette semaine', 'Ce mois', 'Ce trimestre'];
+  sectors: string[] = ['Tous les secteurs', 'Secteur A', 'Secteur B', 'Secteur C'];
+  compliances: string[] = ['Tous', 'Conformes', 'Non conformes'];
+  
+  stats = {
+    totalUsers: 1250,
+    presentThisWeek: 843,
+    regularUsers: 621,
+    compliantUsers: 589
+  };
 }
